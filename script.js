5ad5931d71f7579afc2f8c4091d60ec0ac3effad
Adarsh29-10 <146656899+Adarsh29-10@users.noreply.github.com>
2024-03-27 20:00:15 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+var box = document.querySelectorAll(".div")
+
+box.forEach(function(elem){
+    elem.addEventListener("click", function(){
+        elem.innerHTML = "x"
+    })
+})
+
+
+
+
+
+
+
+// var a = document.querySelector(".a");
+// var b = document.querySelector(".b");
+// var c = document.querySelector(".c");
+// var d = document.querySelector(".d");
+// var e = document.querySelector(".e");
+// var f = document.querySelector(".f");
+// var g = document.querySelector(".g");
+// var h = document.querySelector(".h");
+// var i = document.querySelector(".i");
